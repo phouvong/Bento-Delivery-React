0ffa3a41e23489b680265f85165518b292827b0b
phouvong <48210806+phouvong@users.noreply.github.com>
2024-08-01 11:02:52 +0700

    Update languageList.js

--- src/components/header/top-navbar/language/languageList.js
+++ src/components/header/top-navbar/language/languageList.js
@@ -1,8 +1,15 @@
-import usFlag from "./assets/us.svg";
+import usFlag from "./assets/eng.svg";
+import loFlag from "./assets/lao.svg";
 import arabicFlag from "./assets/arabic-flag-svg.svg";
 import spain from "./assets/spain.png";
 import bangladesh from "./assets/bangladesh.png";
 export const languageList = [
+  {
+    languageName: "ພາສາລາວ",
+    languageCode: "lo",
+    countryCode: "LA",
+    countryFlag: loFlag.src,
+  },
   {
     languageName: "English",
     languageCode: "en",
