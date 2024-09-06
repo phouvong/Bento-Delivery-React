8bfa6b768b14a51f24bfcd15593522b2d9f0539b
phouvong <pphouvong@gmail.com>
2024-09-06 11:52:32 +0700

    remove

--- src/helper-functions/moduleTypes.js
+++
@@ -1,7 +0,0 @@
-export const ModuleTypes = {
-  GROCERY: "grocery",
-  PHARMACY: "pharmacy",
-  ECOMMERCE: "ecommerce",
-  FOOD: "food",
-  PARCEL: "parcel",
-};
--- src/utils/staticCredential.js
+++
@@ -1,8 +0,0 @@
-export const google_client_id = "";
-export const fb_app_id = "";
-export const appleLoginCredential = {
-	appleId: "",
-	serviceId: "",
-	privateKey: "",
-	redirectURI: "",
-};
