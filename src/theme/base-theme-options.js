e1e4793b5f0a3fcd010a0e0a845979961f534e76
phouvong <pphouvong@gmail.com>
2024-09-09 15:44:40 +0700

    Update base-theme-options.js

--- src/theme/base-theme-options.js
+++ src/theme/base-theme-options.js
@@ -253,7 +253,7 @@ export const baseThemeOptions = {
     button: {
       fontWeight: 600,
     },
-    fontFamily: '"Noto Sans Lao Looped", sans-serif',
+    fontFamily: '"Noto Sans Lao Looped","sans-serif"',
     //this is default font size for typography
     body1: {
       fontSize: "0.875rem",
