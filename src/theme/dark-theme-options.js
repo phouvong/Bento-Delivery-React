8aa761e21fd264a99a37ce959e0556adb53137e0
phouvong <48210806+phouvong@users.noreply.github.com>
2024-09-16 13:48:46 +0700

    Update dark-theme-options.js

--- src/theme/dark-theme-options.js
+++ src/theme/dark-theme-options.js
@@ -40,7 +40,7 @@ const foodCardColor = neutral[800];
 const roundStackOne = "rgba(255, 255, 255, 0.04)";
 const roundStackTwo = "rgba(255, 255, 255, 0.06)";
 const primary = {
-  main: "FF4940",
+  main: "#FF4940",
   light: "#909BEF",
   dark: "#1c6641",
   deep: "#026034",
