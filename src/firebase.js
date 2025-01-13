12f12593833b8e2e1aa2cedbb8f49d6901879ea4
phouvong <48210806+phouvong@users.noreply.github.com>
2025-01-13 13:07:40 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import {
 } from "firebase/messaging";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyBV4ueEuzRBy0Yehx-OvUi68pHznhGnT0E",
+  authDomain: "bento-delivery-service.firebaseapp.com",
+  projectId: "bento-delivery-service",
+  storageBucket: "bento-delivery-service.firebasestorage.app",
+  messagingSenderId: "787586896333",
+  appId: "1:787586896333:web:f6fead1c4225e3e0c6b484",
+  measurementId: "G-HP95SH6F7R",
 };
 const firebaseApp = !getApps().length
   ? initializeApp(firebaseConfig)
