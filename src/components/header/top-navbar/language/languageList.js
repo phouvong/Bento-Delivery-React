5a4332dd423cc59296c91068c8139989718d2ae4
phouvong <48210806+phouvong@users.noreply.github.com>
2025-01-14 12:32:19 +0700

    update lao language

--- src/components/header/top-navbar/language/languageList.js
+++ src/components/header/top-navbar/language/languageList.js
@@ -1,4 +1,5 @@
-import usFlag from "./assets/us.svg";
+import usFlag from "./assets/en.png";
+import loFlag from "./assets/lo.png";
 import arabicFlag from "./assets/arabic-flag-svg.svg";
 import spain from "./assets/spain.png";
 import bangladesh from "./assets/bangladesh.png";
@@ -9,18 +10,24 @@ export const languageList = [
     countryCode: "US",
     countryFlag: usFlag.src,
   },
+  {
+    languageName: "ພາສາລາວ",
+    languageCode: "lo",
+    countryCode: "LA",
+    countryFlag: loFlag.src,
+  },
   // {
   //   languageName: "Spanish",
   //   languageCode: "es",
   //   countryCode: "es",
   //   countryFlag: spain.src,
   // },
-  {
-    languageName: "Arabic",
-    languageCode: "ar",
-    countryCode: "SA",
-    countryFlag: arabicFlag.src,
-  },
+  //{
+  //  languageName: "Arabic",
+   // languageCode: "ar",
+   // countryCode: "SA",
+   // countryFlag: arabicFlag.src,
+  //},
   // {
   //   languageName: "Bengali",
   //   languageCode: "bn",
