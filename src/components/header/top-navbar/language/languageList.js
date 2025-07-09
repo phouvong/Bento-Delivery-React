85e313f3626a39a62f5639eb924e0854a6d589ba
phouvong <48210806+phouvong@users.noreply.github.com>
2025-07-09 21:40:45 +0700

    3.2

--- src/components/header/top-navbar/language/languageList.js
+++ src/components/header/top-navbar/language/languageList.js
@@ -1,5 +1,4 @@
-import usFlag from "./assets/en.png";
-import loFlag from "./assets/lo.png";
+import usFlag from "./assets/us.svg";
 import arabicFlag from "./assets/arabic-flag-svg.svg";
 import spain from "./assets/spain.png";
 import bangladesh from "./assets/bangladesh.png";
@@ -10,24 +9,18 @@ export const languageList = [
     countryCode: "US",
     countryFlag: usFlag.src,
   },
-  {
-    languageName: "ພາສາລາວ",
-    languageCode: "lo",
-    countryCode: "LA",
-    countryFlag: loFlag.src,
-  },
   // {
   //   languageName: "Spanish",
   //   languageCode: "es",
   //   countryCode: "es",
   //   countryFlag: spain.src,
   // },
-  //{
-  //  languageName: "Arabic",
-   // languageCode: "ar",
-   // countryCode: "SA",
-   // countryFlag: arabicFlag.src,
-  //},
+  {
+    languageName: "Arabic",
+    languageCode: "ar",
+    countryCode: "SA",
+    countryFlag: arabicFlag.src,
+  },
   // {
   //   languageName: "Bengali",
   //   languageCode: "bn",
--- src/language/i18n.js
+++ src/language/i18n.js
@@ -1,7 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { english } from "./en";
-import { lao } from "./lo";
 import { bengali } from "./bn";
 import { arabic } from "./ar";
 import { spain } from "./es";
@@ -12,9 +11,6 @@ import { spain } from "./es";
 const resources = {
   en: {
     translation: english,
-  },
-  lo: {
-    translation: lao,
   },
   es: {
     translation: spain,
@@ -31,8 +27,8 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "lo", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    fallbackLng: "lo",
+    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    fallbackLng: "en",
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
