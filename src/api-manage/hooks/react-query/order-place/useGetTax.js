8a391e1447d2a5b6b4e24503a25905aeee57277a
MD Hasan Patwary <125332190+patwary6am@users.noreply.github.com>
2025-07-22 09:46:30 +0600

    Update useGetTax.js

--- src/api-manage/hooks/react-query/order-place/useGetTax.js
+++ src/api-manage/hooks/react-query/order-place/useGetTax.js
@@ -1,5 +1,4 @@
 import { useMutation } from "react-query";
-import {rental_get_tax, trip_booking} from "components/home/module-wise-components/rental/rental-api-manage/ApiRoutes";
 import MainApi from "api-manage/MainApi";
 import {tax_api} from "api-manage/ApiRoutes";
 
